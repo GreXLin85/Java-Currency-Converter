d5f5f977b4737487b182882bd3ee594c9c7739f4
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2019-10-25 12:22:48 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        String API_KEY = "API_KEY"; // https://rapidapi.com/natkapral/api/currency-converter5
+        String FROM = "USD";
+        String TO = "TRY";
+        float AMOUNT = 15;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from="+FROM+"&to="+TO+"&amount="+AMOUNT)
+                        .get()
+                        .addHeader("x-rapidapi-host", "currency-converter5.p.rapidapi.com")
+                        .addHeader("x-rapidapi-key", API_KEY)
+                        .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (response.code() == 200){
+            JSONParser parser = new JSONParser();
+
+            Object jsonObj = parser.parse(response.body().string());
+
+            JSONObject jsonObject = (JSONObject) jsonObj;
+
+            JSONObject name = (JSONObject) jsonObject.get("rates");
+            JSONObject asd = (JSONObject)  name.get(TO);
+            String rate_for_amount = (String) asd.get("rate_for_amount");
+            String rate = (String) asd.get("rate");
+            System.out.println(AMOUNT+FROM+"\n"+rate_for_amount+TO+"\nDolar kuru : "+rate);
+
+
+        }else{
+            System.out.println("Bir sorun var!\nSorun kodu : "+response.code());
+        }
+
+    }
+}
